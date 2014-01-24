c6149ff3b8286cc009da136056d7657a23e26701
Ross Masters <ross@rossmasters.com>
2014-01-24 12:51:02 +0000

    Sample config

--- src/config/config.php
+++ src/config/config.php
@@ -2,6 +2,12 @@
 
 return array(
     'environments' => array(
+        'local' => array(
+            'provider' => 'Lasset\Providers\Host',
+            'options' => array(
+                'baseUrl' => '/',
+            ),
+        ),
     ),
-    'default' => null,
+    'default' => 'local',
 );
